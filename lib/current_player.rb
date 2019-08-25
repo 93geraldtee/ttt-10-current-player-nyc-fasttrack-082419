9625a092c1bda246a9528eaa4d506966989458c5
Gerald Tee <93geraldtee@gmail.com>
2019-08-25 16:16:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,7 @@
+#objective is to count how many turns have been played
+
+#strategy 1
+
 def turn_count(board)
   arr = []
   board.each do |position|
@@ -8,3 +12,23 @@ def turn_count(board)
   return arr.length
 end
 
+#strategy 2
+
+#def turn_count(board)
+#  x = 0
+#  board.each do |position|
+#    if position == "X" || position == "0"
+#      x += 1
+#    end
+#    return x
+#end
+
+#objective is to deduce whose turn it is, leveraging the fact that we know how many turns have happened already
+
+#def current_player(board)
+#  if turn_count(board) % 2 == 0 || turn_count(board) == 0
+#    return "X"
+#  else
+#    return #"O"
+#  end
+#end
